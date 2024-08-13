56742dc31d9529b1a20512ccd6eb945f9980008d
nguyenthaibinhduong <114252895+nguyenthaibinhduong@users.noreply.github.com>
2024-08-13 10:52:39 +0700

    Update posts.js [12]

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -6,7 +6,7 @@ exports.getPost = (req, res) => {
     const user_id = req.query.user_id;
     const token = req.cookies.access_token;
     if (!token) { 
-        return res.status(401).json(req.cookies);
+        return res.status(401).json(req.cookie);
     }
     jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, data) => { 
         if (err) return res.status(403).json('Invalid Token');
